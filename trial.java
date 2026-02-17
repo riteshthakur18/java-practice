700e2ff5fd3053892c69858c7bb4a184ac667f8a
riteshthakur18 <riteshthakur2110@gmail.com>
2026-02-17 21:40:01 +0530

    Updated program

--- trial.java
+++ trial.java
@@ -4,5 +4,6 @@ public static void main(String[]args)
 {
 System.out.println("Hello guys i am Ritesh");
 System.out.println("My first java program on github");
+  System.out.println("here i type this line because i want to know how to edit our file");
 }
 }
